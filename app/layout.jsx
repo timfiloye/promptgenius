32ab7127a15cf9df08c2e61cbfcfe15b1b548fbf
timfiloye <timfiloye69@gmail.com>
2024-08-22 01:10:17 -0700

    Updated

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata ={
+    title: "Promptgenius",
+    description: "Discover and share Ai prompts.",
+}
+
+const RootLayout = ({children}) => {
+  return (
+    
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className='gradient'/>
+            </div>
+
+            <main className='app'>
+                <Nav/>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
