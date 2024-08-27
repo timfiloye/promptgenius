3b9ff7bac5841e30bbc3812ba433a5b3a842e1f6
timfiloye <timfiloye69@gmail.com>
2024-08-27 09:30:46 -0700

    Updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Googleprovider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 import { connectToDB } from "@utils/database";
-
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
@@ -12,18 +12,34 @@ const handler = NextAuth({
         })
     ],
     async session({session}) {
+        const sessionUser = await user.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session
     },
     async signIn({profile}) {
         try {
             await connectToDB();
 
             //check if a user already exists
-
+            const userExists = await user.findOne({
+                email: profile.email
+            })
             //if not, create a new user
-
+            if(!userExists) {
+                await user.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                })
+            }
             return true;
         } catch (error) {
+            console.log(error);
+            
             return false;
         }
     }
---
+++ models/user.js
@@ -0,0 +1,20 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = newSchema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required'],
+    },
+    username: {
+        required: [true, 'username is required'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String,
+    }
+});
+
+const User = models.user || model("User", UserSchema);
+
+export default User;
